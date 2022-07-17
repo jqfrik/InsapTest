0f09018beaaa810e5388e7d4d80c611240f763a5
Defpmportal20 <defpmportal.2.0@gmail.com>
2022-07-17 22:01:38 +0300

    q

--- InsapTestovoe/Program.cs
+++ InsapTestovoe/Program.cs
@@ -5,10 +5,9 @@ var fileCreator = new FileCreator();
 var fileName = fileCreator.CreateFileWithData(10000,10000);
 var data = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), fileName));
 var dic = new ConcurrentDictionary<char, int>();
-var parsed = data.Select(x => new String(x.OrderBy(x => x).ToArray())).ToArray();
-Parallel.For(0, parsed.Count(), (i =>
+Parallel.For(0, data.Count(), (i =>
 {
-    var word = parsed[i];
+    var word = data[i];
     foreach (var ch in word)
     {
         if (dic.TryGetValue(ch, out var result))
